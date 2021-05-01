f3d3ea9876a8210f7879abda6e6ad4089f99fe33
Muhammad Karbalaee Shabani <muhammad.ksht@gmail.com>
2021-05-01 19:14:31 +0430

    semaphor implemented

--- src/main/java/sbu/cs/multithread/semaphor/SourceController.java
+++ src/main/java/sbu/cs/multithread/semaphor/SourceController.java
@@ -1,14 +1,20 @@
 package sbu.cs.multithread.semaphor;
 
 
-public class SourceController {
+import java.util.concurrent.Semaphore;
 
-    public static void main(String[] args) {
-        Chef chef1 = new Chef("chef1");
-        Chef chef2 = new Chef("chef2");
-        Chef chef3 = new Chef("chef3");
-        Chef chef4 = new Chef("chef4");
-        Chef chef5 = new Chef("chef5");
+public class SourceController
+{
+
+    public static void main(String[] args)
+    {
+        Semaphore semaphore = new Semaphore(2);
+
+        Chef chef1 = new Chef("chef1",semaphore);
+        Chef chef2 = new Chef("chef2",semaphore);
+        Chef chef3 = new Chef("chef3",semaphore);
+        Chef chef4 = new Chef("chef4",semaphore);
+        Chef chef5 = new Chef("chef5",semaphore);
         chef1.setPriority(1);
         chef2.setPriority(1);
         chef3.setPriority(1);
